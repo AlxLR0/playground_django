555f2c2e6d7567600f7aeb03f4cc3b5ef40cd655
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-04-17 15:33:47 -0600

    feat: add Author and Genre models and update Book model with isbn, pages, and genres fields

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -8,12 +8,20 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    pages = models.IntegerField()
-    isbn = models.CharField(max_length=50)
+    pages = models.IntegerField(default=0)
+    isbn = models.CharField(max_length=50, default='0000')
+    genres = models.ManyToManyField(Genre, related_name='books')
+
 
     def __str__(self):
         return self.title
