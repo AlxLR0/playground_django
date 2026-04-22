eeccdc11ad4881f8f9c96bfd905e82c0949b2e0a
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-04-22 15:38:54 -0600

    feat: add Loan and Recommendation models and update review fields

--- minilibrary/admin.py
+++ minilibrary/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author, Book, Genre, BookDetail, Review, Loan, Recomendation
 
 # Register your models here.
+
+#personalizar campos para el django admin
+
+#este reviewinline permite agregar una reseña al libro desde la misma interface de libro 
+class ReviewInline(admin.TabularInline):
+    model = Review # aqui se manda a llamar al modelo review
+    extra = 1 #sirve para mostrar un cuadro en blanco mas extra de los que ya hay
+
+class BookDetailInline(admin.StackedInline):
+    model = BookDetail
+    can_delete = False #evita que se borre el detalle del libro
+    verbose_name_plural = 'Detalle del libro'#esto sirve para ponerle un titulo al cuadro
+    
+    
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline, BookDetailInline]#aqui se ponen los cuadros que permiten agregar informacion relacionada con el libro
+    list_display = ('title', 'author', 'pages','publication_date')#columnas que se muestran en la lista
+    search_fields = ('title', 'author__name')#poner un buscador, usar __ para buscar en campos relacionados
+    list_filter = ('author', 'genres', 'publication_date')#poner filtros para ordenar la lista 
+    date_hierarchy = 'publication_date'#sirve para poner un buscador por fechas
+    
+
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre)
+admin.site.register(BookDetail)
+admin.site.register(Review)
+admin.site.register(Loan)
+admin.site.register(Recomendation)
--- minilibrary/models.py
+++ minilibrary/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 class Author(models.Model):
@@ -21,6 +23,7 @@ class Book(models.Model):
     pages = models.IntegerField(default=0)
     isbn = models.CharField(max_length=50, default='0000')
     genres = models.ManyToManyField(Genre, related_name='books')
+    recomended_by = models.ManyToManyField(get_user_model(), through='Recomendation', related_name='recomendations')
 
 
     def __str__(self):
@@ -34,3 +37,32 @@ class BookDetail(models.Model):
 
     def __str__(self):
         return self.book.title
+
+class Review(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.user} - {self.book.title} ({self.rating}/5)'
+    
+class Loan(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='loans')
+    loan_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user} - {self.book.title} ({'devuelto' if self.is_returned else 'prestado'})'
+
+class Recomendation(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='recomendations')
+    recomendation_date = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('user', 'book')
